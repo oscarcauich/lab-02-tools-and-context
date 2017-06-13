f6ea271318ba7e7a8b3332278b63de15e8ea5cc4
Oscar Cauich <ocauich@gravitypayments.com>
2017-06-13 15:20:52 -0700

    tests for map functoin done

--- lab-oscar/lib/fp.js
+++ lab-oscar/lib/fp.js
@@ -2,7 +2,11 @@
 
 const fp = module.exports = {};
 
-fp.map = () => {};
+fp.map = (list, callback) => {
+  if(list !== null)
+    return Array.prototype.map.call(list, callback);
+  return null;
+};
 fp.filter = () => {};
 fp.reduce = () => {};
 fp.concat = () => {};
--- lab-oscar/test/fp-test.js
+++ lab-oscar/test/fp-test.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const expect = require('expect');
+const fp = require('../lib/fp.js');
+
+describe('should return an list array', () =>{
+
+  it('should return an upperCase list of Array', () => {
+    let result = fp.map(['a','b','c'], n => n.toUpperCase());
+    expect(result).toEqual(['A','B','C']);
+  });
+
+  it('should return an array', () => {
+    let result = fp.map('oscar', n => n);
+    expect(result).toEqual(['o','s','c','a','r']);
+  });
+  it('should be return null', () => {
+    let result = fp.map(null, n => n);
+    expect(result).toEqual(null);
+  });
+});
