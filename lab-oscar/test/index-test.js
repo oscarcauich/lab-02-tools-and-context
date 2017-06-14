8593fe2be3a8cf57cc7f8e22b151bf046e21ac5e
Oscar Cauich <ocauich@gravitypayments.com>
2017-06-14 00:24:21 -0700

    test for index.js file is not yet working

---
+++ lab-oscar/test/index-test.js
@@ -0,0 +1,12 @@
+// 'use strict';
+//
+// const expect = require('expect');
+// const main = require('../index.js');
+//
+// describe('testing index.js file', () => {
+//   it('should return an array', () => {
+//     let result = main('oscar');
+//     expect(result).toEqual(['O','S','C','A','R']);
+//   });
+// });
+//I couldn't get the test to work!
